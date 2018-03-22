78e0b5a2fcf8113db135f1ab5d27bef6243b19c1
Alfonso Soldado Caro <32943650+AlfonsoSoldado@users.noreply.github.com>
2018-03-22 20:25:57 +0100

    fix: translation of labels to spanish

--- principal/models.py
+++ principal/models.py
@@ -11,14 +11,14 @@ class Service(models.Model):
         ('Recados', 'Recados'),
         ('Sin especificar', 'Sin especificar'),
     )
-    name = models.CharField(max_length=100)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.TextField()
-    created = models.DateField(auto_now_add=True)
-    price = models.DecimalField(max_digits=20, decimal_places=2)
-    avaliability = models.IntegerField()
-    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
-    offerer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="offerer", null=True, blank=True)
+    name = models.CharField(verbose_name=('Nombre'), max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=('Autor'))
+    description = models.TextField(verbose_name=('Descripción'))
+    created = models.DateField(verbose_name=('Fecha de creación'), auto_now_add=True)
+    price = models.DecimalField(verbose_name=('Precio'), max_digits=20, decimal_places=2)
+    avaliability = models.IntegerField(verbose_name=('Disponibilidad'))
+    category = models.CharField(verbose_name=('Categoría'), max_length=50, choices=CATEGORY_CHOICES)
+    offerer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="offerer", null=True, blank=True, verbose_name=('Solicitante'))
 
     def __str__(self):
         return f'{self.name}'
@@ -37,14 +37,14 @@ class UserDetails(models.Model):
     )
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="El número debe seguir el siguiente patrón: '+999999999'. Máximo 15 dígitos.")
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    birthday = models.DateField()
-    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
-    postalAddress = models.CharField(max_length=100)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    occupation = models.CharField(max_length=100)
-    photo = models.URLField(max_length=600)
-    socialReferences = models.CharField(null=True,blank=True,max_length=600)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=('Usuario'))
+    birthday = models.DateField(verbose_name=('Fecha de nacimiento'))
+    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True, verbose_name=('Teléfono'))
+    postalAddress = models.CharField(max_length=100, verbose_name=('Domicilio'))
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name=('Género'))
+    occupation = models.CharField(max_length=100, verbose_name=('Profesión'))
+    photo = models.URLField(max_length=600, verbose_name=('Foto'))
+    socialReferences = models.CharField(null=True,blank=True,max_length=600, verbose_name=('Redes Sociales'))
 
     def __str__(self):
         return f'{self.user.username}'
