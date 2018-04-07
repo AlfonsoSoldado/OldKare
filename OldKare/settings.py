a46a2e6c18aba81181882dca1298a609c52b2129
danirguez <32953186+danirguez@users.noreply.github.com>
2018-04-07 15:03:08 +0200

    fix: ya va la internacionalización!

--- OldKare/settings.py
+++ OldKare/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'en'
 
 LANGUAGES = (
     ('en', _('English')),
