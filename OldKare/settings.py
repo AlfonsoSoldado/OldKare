76cb9af04d34a9cb2693d00b9b53aecb1a9d9445
Alfonso Soldado Caro <32943650+AlfonsoSoldado@users.noreply.github.com>
2018-05-16 20:06:17 +0200

    fix: debug = false

--- OldKare/settings.py
+++ OldKare/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')hemo81z&(8y&1ux*l%d00f0fwsy2o832rlcbvpsj6jw@#_m!y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = '/login'
 
